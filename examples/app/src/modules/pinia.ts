efda29b82ddb398525d0298ce6803338f9938600
MARX Dominic <dominic.marx@axians.de>
2023-07-12 14:50:37 +0200

    fix: remove console log

--- examples/app/src/modules/pinia.ts
+++ examples/app/src/modules/pinia.ts
@@ -6,10 +6,8 @@ import { type UserModule } from '~/types'
 export const install: UserModule = ({ isClient, initialState, app }) => {
   const pinia = createPinia()
 
-  if (isClient) {
-    console.log(initialState.pinia)
+  if (isClient)
     pinia.state.value = (initialState.pinia) || {}
-  }
 
-  else { initialState.pinia = pinia.state.value }
+  else initialState.pinia = pinia.state.value
 }
